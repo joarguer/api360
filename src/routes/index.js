2a1b343dbeabe7c0a7eecf32e5e5929f49a4a621
jorge <joarguer@gmail.com>
2022-07-01 08:16:09 -0500

    ocho

--- src/routes/index.js
+++ src/routes/index.js
@@ -126,7 +126,7 @@ router.post('/registraduria', async (req, res) => {
         )
         const browser = await puppeteer.launch({
           headless: true,
-          args: ['--no-sandbox']
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
     
